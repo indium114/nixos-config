2b8a273f43e9cb309368dac6a938e7b335e57f19
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-03-06 05:25:00 +0200

    Enable joycond properly

--- configs/packages/gaming.nix
+++ configs/packages/gaming.nix
@@ -12,12 +12,12 @@
     osu-lazer-bin
     classicube
     wineWow64Packages.stable
-    joycond
     joycond-cemuhook
     cemu
   ];
 
   programs.steam.enable = true;
   programs.gamemode.enable = true;
+  services.joycond.enable = true;
 
 }

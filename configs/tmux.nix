dbe34a63bdbc04a0e62d7802f604fb7bbc40a273
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-03-09 19:46:13 +0200

    Tmux statusline config

--- configs/tmux.nix
+++ configs/tmux.nix
@@ -42,6 +42,45 @@
       bind w kill-window
       bind Tab next-window
       bind r command-prompt "rename-window %%"
+
+      ### Statusline stuff
+      set -g status-interval 5
+
+      # Style
+      set -g status-style "bg=#1e1e2e,fg=#cdd6f4"
+      set -g status-justify left
+
+      # Remove default separators
+      set -g window-status-separator ""
+
+      # Left side length
+      set -g status-left-length 40
+
+      set -g status-left "\
+      #[bg=#89b4fa] #[bg=#1e1e2e] \
+      #[fg=#89b4fa,bold]#H \
+      #[default] "
+
+      set -g window-status-format "\
+      #[bg=#1e1e2e,fg=#cdd6f4] \
+      #[bg=#313244,fg=#cdd6f4] #I #W \
+      #[bg=#1e1e2e,fg=#313224] "
+
+      set -g window-status-current-format "\
+      #[bg=#1e1e2e,fg=#1e1e2e,bold] \
+      #[bg=#89b4fa,fg=#1e1e2e,bold] #I #W \
+      #[bg=#1e1e2e,fg=#89b4fa] "
+      set -g status-right-length 80
+
+      set -g status-right "\
+      #[bg=#cba6f7,fg=#1e1e2e]  #(tmux display-message -p '#{pane_current_command}') \
+      #[default] \
+      #[bg=#a6e3a1,fg=#1e1e2e]  #S \
+      #[default] \
+      #[bg=#74c7ec,fg=#1e1e2e]  #(uptime | sed 's/^[^,]*up *//; s/, *[[:digit:]]* user.*//; s/ day.*, */d /; s/ hr\\(s*\\).*/h/; s/ min\\(s*\\).*/m/; s/ sec\\(s*\\).*/s/; s/\\([0-9]\\{1,2\\}\\):\\([0-9]\\{1,2\\}\\)/\\1h \\2m/') \
+      #[default]"
+
+      set -g status-style "bg=#{?client_prefix,#f38ba8,#1e1e2e},fg=#cdd6f4"
     '';
   };
 }

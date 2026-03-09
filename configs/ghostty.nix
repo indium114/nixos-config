8f902feb214ea6dcd9e66763901711f951bf74a4
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-03-09 21:24:37 +0200

    Fix ghostty configuration

--- configs/ghostty.nix
+++ configs/ghostty.nix
@@ -7,11 +7,11 @@
   programs.ghostty = {
     enable = true;
     settings = {
-      theme = "catppuccin-mocha";
+      theme = "Catppuccin Mocha";
       font-family = "Martian Mono Cn Md";
       background-opacity = 0.78;
-      window-padding-x = 8.8;
-      widnow-padding-y = 8.8;
+      window-padding-x = "8,8";
+      window-padding-y = "8,8";
       command = "tmux";
       bell-features = [ "no-audio" "attention" ];
     };

9b67c8caaa7365770df7c8c617b260a0552ecf40
indium114 <156162907+indium114@users.noreply.github.com>
2026-03-31 14:53:50 +0200

    nixos(modules/virtualisation.nix): remove virtualbox

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -5,10 +5,6 @@
 {
 
   virtualisation.libvirtd.enable = true;
-  virtualisation.virtualbox.host = {
-    enable = true;
-    enableExtensionPack = true;
-  };
   programs.virt-manager.enable = true;
 
 }

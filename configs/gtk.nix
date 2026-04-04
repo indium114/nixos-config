71e5ca284ece407ff546d8966a86d93f7d92c58c
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-04 20:28:39 +0200

    home-manager(configs/gtk.nix): temporarily disable gtk theme

--- configs/gtk.nix
+++ configs/gtk.nix
@@ -7,10 +7,10 @@
 
   gtk = {
     enable = true;
-    cursorTheme = {
-      name = "catppuccin-mocha-dark";
-      package = pkgs.catppuccin-cursors.mochaMauve;
-    };
+    # cursorTheme = {
+    #   name = "catppuccin-mocha-dark";
+    #   package = pkgs.catppuccin-cursors.mochaMauve;
+    # };
   };
 
 }

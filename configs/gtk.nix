195724f718063a0f1d4df9fa977f3a4bd562963d
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-05 12:52:39 +0200

    home-manager(configs/gtk.nix): change cursorTheme

--- configs/gtk.nix
+++ configs/gtk.nix
@@ -7,10 +7,10 @@
 
   gtk = {
     enable = true;
-    # cursorTheme = {
-    #   name = "catppuccin-mocha-dark";
-    #   package = pkgs.catppuccin-cursors.mochaMauve;
-    # };
+    cursorTheme = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+    };
     iconTheme = {
       package = pkgs.papirus-icon-theme;
       name = "Papirus-Dark";

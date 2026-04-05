780b93e1bc5c564062eaabe077e125275faf31ff
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-05 12:23:55 +0200

    home-manager(configs/helix/sorbet.toml, configs/helix.nix): add inline annotations highlights to helix theme

--- configs/helix.nix
+++ configs/helix.nix
@@ -6,6 +6,7 @@
 {
 
   programs.helix.enable = true;
+  xdg.configFile."helix/themes/sorbet.toml".source = lib.mkForce ./helix/sorbet.toml;
   xdg.configFile."helix/runtime/queries/markdown/highlights.scm".source = lib.mkForce ./helix/highlights.scm;
   xdg.configFile."helix/config.toml".source = lib.mkForce ./helix/config.toml;
   xdg.configFile."helix/languages.toml".source = lib.mkForce ./helix/languages.toml;

2d5e760c2cc5cfaf3edc20403afe091a7440ea9e
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-05 12:14:36 +0200

    home-manager(configs/nushell.nix, configs/nushell/catppuccin_mocha.nu, configs/nushell/config.nu, config/nushell/sorbet.nu): theme nushell

--- configs/nushell.nix
+++ configs/nushell.nix
@@ -5,10 +5,10 @@
 {
 
   xdg.configFile."nushell/aliases.nu".source = ./nushell/aliases.nu;
-  xdg.configFile."nushell/catppuccin_mocha.nu".source = ./nushell/catppuccin_mocha.nu;
   xdg.configFile."nushell/completers.nu".source = ./nushell/completers.nu;
   xdg.configFile."nushell/config.nu".source = ./nushell/config.nu;
   xdg.configFile."nushell/direnv.nu".source = ./nushell/direnv.nu;
+  xdg.configFile."nushell/sorbet.nu".source = ./nushell/sorbet.nu;
   xdg.configFile."nushell/starship.nu".source = ./nushell/starship.nu;
   xdg.configFile."nushell/tv.nu".source = ./nushell/starship.nu;
   xdg.configFile."nushell/zoxide.nu".source = ./nushell/zoxide.nu;

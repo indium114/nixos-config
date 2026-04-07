394e12b6507e84ade87726b2e3e71e0c3090e9a7
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-07 14:44:50 +0200

    home-manager(configs/helix.nix): format with nixfmt

--- configs/helix.nix
+++ configs/helix.nix
@@ -7,7 +7,8 @@
 
   programs.helix.enable = true;
   xdg.configFile."helix/themes/sorbet.toml".source = lib.mkForce ./helix/sorbet.toml;
-  xdg.configFile."helix/runtime/queries/markdown/highlights.scm".source = lib.mkForce ./helix/highlights.scm;
+  xdg.configFile."helix/runtime/queries/markdown/highlights.scm".source =
+    lib.mkForce ./helix/highlights.scm;
   xdg.configFile."helix/config.toml".source = lib.mkForce ./helix/config.toml;
   xdg.configFile."helix/languages.toml".source = lib.mkForce ./helix/languages.toml;
 

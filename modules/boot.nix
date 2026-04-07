c44793a29fb45ad7b688cf84040d40fa41b68734
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-07 15:28:50 +0200

    nixos(modules/boot.nix): switch to limine bootloader

--- modules/boot.nix
+++ modules/boot.nix
@@ -5,7 +5,7 @@
 
 {
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.limine.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 

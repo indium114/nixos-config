0ecd744c5e2f2e3067375ce29a9642050e8ce1af
indium114 <156162907+indium114@users.noreply.github.com>
2026-05-14 17:50:25 +0200

    nixos(modules/networking.nix): enable yggdrasil network

--- modules/networking.nix
+++ modules/networking.nix
@@ -12,4 +12,15 @@
     enable = true;
   };
 
+  services.yggdrasil = {
+    enable = true;
+    settings = {
+      Peers = [
+        "tls://yggdrasil.deavmi.assigned.network:2001"
+        "tls://yg-mel.magicum.net:23800"
+        "tls://yg-syd.magicum.net:23700"
+      ];
+    };
+  };
+
 }

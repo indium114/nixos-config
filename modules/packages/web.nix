c5cb10675d97d4f3d8e5429ad50719c5ef21f883
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-27 21:29:11 +0200

    nixos(modules/packages/web.nix): install wireshark the proper way

--- modules/packages/web.nix
+++ modules/packages/web.nix
@@ -17,12 +17,16 @@
     dino
     newsflash
     hydroxide
-    wireshark
   ];
 
   # nixpkgs.config.permittedInsecurePackages = [
   #   "librewolf-${pkgs.librewolf.version}"
   #   "librewolf-unwrapped-${pkgs.librewolf-unwrapped.version}"
   # ];
+
+  programs.wireshark = {
+    enable = true;
+    dumpcap.enable = true;
+  };
 
 }

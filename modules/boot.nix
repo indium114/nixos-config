b76ab4f8baac91a26726fc1c71566e0e44bcf2ca
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-30 11:14:22 +0200

    nixos(modules/boot.nix): switch back to linux-zen kernel

--- modules/boot.nix
+++ modules/boot.nix
@@ -7,7 +7,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.tmp.cleanOnBoot = true;
 
 }

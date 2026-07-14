11689ae6638f8b9ef6597cea39eece0d390af5e2
indium114 <156162907+indium114@users.noreply.github.com>
2026-07-14 12:50:19 +0200

    home-manager(configs/nushell.nix): fix path to tv.nu
    no wonder the tv/nushell integration hadn't been working :P

--- configs/nushell.nix
+++ configs/nushell.nix
@@ -11,7 +11,7 @@
   xdg.configFile."nushell/direnv.nu".source = ./nushell/direnv.nu;
   xdg.configFile."nushell/keybinds.nu".source = ./nushell/keybinds.nu;
   xdg.configFile."nushell/starship.nu".source = ./nushell/starship.nu;
-  xdg.configFile."nushell/tv.nu".source = ./nushell/starship.nu;
+  xdg.configFile."nushell/tv.nu".source = ./nushell/tv.nu;
   xdg.configFile."nushell/zoxide.nu".source = ./nushell/zoxide.nu;
 
 }
